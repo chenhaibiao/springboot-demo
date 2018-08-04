0cfb98ab8a71abcbee8f5f71d7b49659a8ed2a8e
chenhaibiao <13161027080@163.com>
2018-08-04 11:47:24 +0800

    debug

--- src/main/java/com/springboot/Application.java
+++ src/main/java/com/springboot/Application.java
@@ -1,6 +1,8 @@
 package com.springboot;
-import org.mybatis.spring.annotation.MapperScan; 
-import org.springframework.boot.SpringApplication; 
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication; 
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -12,9 +14,12 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableTransactionManagement //如果mybatis中service实现类中加入事务注解，需要此处添加该注解
 @MapperScan("com.springboot.mapper") //配置扫描mapper接口的地址
 public class Application {
-	
+
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) { 
 		SpringApplication.run(Application.class, args);
+		logger.info("SpringBoot start Success");
 	}
 }
 
